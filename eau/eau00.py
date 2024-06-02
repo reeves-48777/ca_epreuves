19854369995f0170cce4e19b564668df35895cd4
Reeves-48777 <=>
2024-06-02 18:18:07 +0200

    Résolution de l'exercice 1 de l'épreuve de l'eau (ENFIN)
    J'ai pas mal galéré parce que je n'avais pas bien lu la consigne, qui parlait des ordres croissants, je pensais qu'il fallait vérifier qu'un nombre n'avait pas déjà été rentré dans un autre ordre

--- eau/eau00.py
+++ eau/eau00.py
@@ -1,29 +1,22 @@
-def sign_number(number: str):
-    sum = 0
-    for part in number:
-        sum += ord(part)
-    return sum
-
 # on controle les centaines, dizaines et unités
 def is_valid_number(c, d, u):
     if c == d == u:
         return False
     if c == d or d == u or c == u:
         return False
+    if not c < d < u:
+        return False
     return True
 
-
 def create_combinaisons():
-    resultat = dict()
+    resultat = list()
     for i in "0123456789":
         for j in "0123456789":
             for k in "0123456789":
                 if is_valid_number(i,j,k):
                     number = f"{i}{j}{k}"
-                    print(number)
-                    sig = sign_number(number)
-                    if not sig in resultat.keys():
-                        resultat[sig] = number
-    return ",".join([combinaison for combinaison in resultat.values()])
+                    resultat.append(number)
+                    
+    return ",".join(resultat)
 
 print(create_combinaisons())
