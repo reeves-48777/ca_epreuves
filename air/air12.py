01756174d6cbd4183a75839b7827d3006eeb834b
Thomas Durand <durandthomas@pm.me>
2024-06-24 14:46:44 +0200

    résolution de l'exercice 12 de l'épreuve de l'air

---
+++ air/air12.py
@@ -0,0 +1,46 @@
+import sys
+import random
+
+def parse_args():
+    argc = len(sys.argv)
+    if argc < 2:
+        print("error")
+        exit(1)
+    unsorted_numbers = list()
+
+    for el in sys.argv[1:]:
+        try:
+            unsorted_numbers.append(int(el))
+        except:
+            print("error")
+            exit(2)
+    return unsorted_numbers
+
+def swap(arr, a, b):
+    temp = arr[a]
+    arr[a] = arr[b]
+    arr[b] = temp
+
+def partition(arr, left, right, pivot):
+    swap(arr, pivot, right)
+    j = left
+    for i in range(left, right):
+        if arr[i] <= arr[right]:
+            swap(arr, i, j)
+            j = j + 1
+
+    swap(arr, right, j)
+
+    return j
+
+def quick_sort(arr, left, right):
+    if left < right:
+        pivot = random.randrange(left, right)
+        pivot = partition(arr, left, right, pivot)
+        quick_sort(arr, left, pivot - 1)
+        quick_sort(arr, pivot + 1, right)
+
+
+elements = parse_args()
+quick_sort(elements, 0, len(elements) - 1)
+print(elements)
