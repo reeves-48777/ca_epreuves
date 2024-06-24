d64624e9a5659a7088fe906336c90068eaafa3ec
Thomas Durand <durandthomas@pm.me>
2024-06-24 02:36:21 +0200

    résolution de l'exercice 8 de l'épreuve de l'air

--- air/air08.py
+++ air/air08.py
@@ -1,43 +1,53 @@
 import sys
 
 def parse_args():
-	if len(sys.argv) < 2 or not "fusion" in sys.argv:
-		print("error")
-		exit(1)
+    if len(sys.argv) < 2 or not 'fusion' in sys.argv:
+        print("error")
+        exit(1)
 
-	try:
-		tab1 = list()
-		tab2 = list()
+    try:
+        tab1 = list()
+        tab2 = list()
 
-		insert_new = False
-		for el in sys.argv[1:]:
-			if insert_new:
-				tab2.append(int(el))
-			else:
-				tab1.append(int(el))
-			if el == "fusion":
-				insert_new = True
-	except:
-		print("error")
-		exit(2)
+        insert_new = False
+        for el in sys.argv[1:]:
+            if el == "fusion":
+                insert_new = True
+            elif insert_new:
+                tab2.append(int(el))
+            elif not insert_new:
+                tab1.append(int(el))
+    except:
+        print("error")
+        exit(2)
 
-	return tab1, tab2
+    return tab1, tab2
 
-
-# on veut insérer un élément dans un tableau tout en le gardant trié
 def sorted_insert(first: list[int], second: list[int]):
-	new_array = list()
-	for i in range(len(first) - 1):
-		new_array.append(first[i])
-		for el in second:
-			if first[i] < el < first[i + 1]:
-				new_array.append(el)
+    if len(first) == 0:
+        return second
+    elif len(second) == 0:
+        return first
+    else:
+        array = list()
+        i = 0
+        j = 0
+        while i < len(first) and j < len(second):
+            if first[i] < second[j]:
+                array.append(first[i])
+                i += 1
+            elif second[j] < first[i]:
+                array.append(second[j])
+                j += 1
 
-	new_array.append(first[len(first) - 1])
+        while i < len(first):
+            array.append(first[i])
+            i += 1
+        while j < len(second):
+            array.append(second[j])
+            j += 1
+        return array
 
-	return new_array
-
-
-arr1, arr2 = parse_args()
+arr1, arr2 = parse_args();
 
 print(sorted_insert(arr1, arr2))
